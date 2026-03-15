aa514221d1171cdaa745c0ae4725e2ebcf8eba61
Miller <millerddmr@gmail.com>
2026-03-15 12:06:15 -0600

    Fix ssr false dynamic import constraint in checkout page

---
+++ src/app/checkout/CheckoutWrapper.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const CheckoutClient = dynamic(() => import("./CheckoutClient"), {
+    ssr: false,
+});
+
+export default function CheckoutWrapper() {
+    return <CheckoutClient />;
+}
--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,15 +1,11 @@
-import dynamic from "next/dynamic";
 import { Metadata } from "next";
+import CheckoutWrapper from "./CheckoutWrapper";
 
 export const metadata: Metadata = {
     title: 'Checkout | Granite Defense Systems',
     description: 'Securely checkout your order at Granite Defense Systems.',
 };
 
-const CheckoutClient = dynamic(() => import("./CheckoutClient"), {
-    ssr: false,
-});
-
 export default function CheckoutPage() {
-    return <CheckoutClient />;
+    return <CheckoutWrapper />;
 }
